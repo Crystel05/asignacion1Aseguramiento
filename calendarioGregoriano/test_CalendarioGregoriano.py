2612e4d5c75350f69232647e49a3f6b27dfd10f5
Yerling Pérez Álvarez <perez29yerling@gmail.com>
2022-06-13 18:32:17 -0600

    Update test_CalendarioGregoriano.py

--- calendarioGregoriano/test_CalendarioGregoriano.py
+++ calendarioGregoriano/test_CalendarioGregoriano.py
@@ -6,6 +6,13 @@ import CalendarioGregoriano as cg
 
 class TestCalendarioGregoriano(unittest.TestCase):
 
+    def test_fecha_futura(self):
+        self.assertEqual(cg.fecha_futura((2022, 10, 31), 25), (2022, 11, 25))
+        self.assertEqual(cg.fecha_futura((2022, -10, 5), 4), "La fecha ingresada no es válida")
+        self.assertEqual(cg.fecha_futura((2150, 5), 10), "La fecha ingresada no es válida")
+        self.assertEqual(cg.fecha_futura((2500, -1), -10.5), "La fecha ingresada no es válida")
+        self.assertEqual(cg.fecha_futura((2022, 5, 15), 10.5), "El número de días tiene que ser mayor o igual a 0")
+
     def test_edad_al(self):
         self.assertEqual(cg.edad_al((1999, 7, 29), (2005, 10, 15)), (6, 2, 16))
         self.assertEqual(cg.edad_al((-19, 12, 1997), (2009, 6, 40)), "Alguna de las fechas ingresadas no es válida")
